dbcc8c312cf45a13036475d705770852bdc76246
Yu Chen Hou <houyuchen66@gmail.com>
2015-12-02 16:44:56 -0500

    show sender name in beam header

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -7,7 +7,7 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
 	if (request.action === "showMessage"){
 		console.log("showing message", request.message);
 		var messageDiv = $("<div>", {id:"beam-messageDiv", class:"beam-messageDiv visible"});
-		messageDiv.text(request.message); 
+		messageDiv.text("Beam from "+ request.fromUser + ": " + request.message); 
 		var closeBeamMessage = $("<span id='beam-closeMessage'>X</span>");
 		closeBeamMessage.click(function(){
 			messageDiv.fadeOut(600, function(){
